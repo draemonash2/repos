954757cee49de80c2afc63ba80819afc336c25f5
Tatsuya Endo <draemon_ash3@yahoo.co.jp>
2026-04-09 07:07:05 +0900

    mod

--- git_pull_all.bat
+++ git_pull_all.bat
@@ -1,3 +1,10 @@
 @echo off
-git submodule update --remote
+::git submodule update --remote
+
+set cur_dir=%~dp0
+
+cd %cur_dir%codes		& git checkout master & git pull
+cd %cur_dir%github_io	& git checkout master & git pull
+cd %cur_dir%other		& git checkout master & git pull
+
 pause
